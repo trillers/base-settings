abfdb1ba55875128e26b63a7703fdedb83ceb7d8
henryleu <henryleu@126.com>
2016-01-26 12:30:55 +0800

    nci

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        name: 'ci'
+        name: 'nci'
     },
 
     logging: {
@@ -10,7 +10,7 @@ module.exports = {
 
     redis: {
         mode: 'single',
-        host: '121.42.61.180',
+        host: '114.215.91.211',
         port: 6379,
         auth: 'trillers'
     },
@@ -18,9 +18,9 @@ module.exports = {
     mongo: {
         host: "114.215.91.211",
         port: 27017,
-        db: "wenode-ci",
-        username: 'wenode-ci',
-        password: 'wenode-ci'
+        db: "note-ci",
+        username: 'note-ci',
+        password: 'note-ci'
     },
 
     rabbitmq: {
