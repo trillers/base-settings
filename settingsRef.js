95d1a7a457e116a6e3899c3d2ea2995e8143311f
leekangtaqi <leekangtaqi@sina.com>
2016-02-18 14:32:24 +0800

    ok

---
+++ settingsRef.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        name: 'prd'
+    },
+
+    logging: {
+        reloadSecs: 0, //INFO: set 0 could let nodeunit tests which use log4js exit properly
+        level: 'INFO'
+    },
+
+    redis: {
+        mode: 'single',
+        host: '127.0.0.1',
+        port: 6379,
+        auth: 'trillers'
+    },
+
+    mongo: {
+        host: "114.215.91.211",
+        port: 27017,
+        db: "wenode",
+        username: 'wenode',
+        password: 'wenode'
+    },
+
+    rabbitmq: {
+        host: "114.215.91.211",
+        port: 5672,
+        vhost: 'wenode_prd',
+        username: 'wenode',
+        password: 'wen0dep@ssw0rd!'
+    }
+};
